3485da61178ef9b3f5bf50bcc89b15480a025977
anna.ootani <anlyche0610@icloud.com>
2023-01-09 13:22:50 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,33 +29,14 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager  em = DBUtil.createEntityManager();//これからDBの操作をしますよ～っておまじない
-        //Springが用意するトランザクションでは対応できない場合に、明示的トランザクションを使うgetTransaction
-        //トランザクション処理⇒処理がどこかでうまくいかなかったら全体の処理がキャンセルになる
-        em.getTransaction().begin();//ここから登録とかの処理を行いますよ～って感じの文
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        // Messageのインスタンスを生成
-        Message m = new Message();
+        //おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
 
-        // mの各フィールドにデータを代入
-        String title = "taro";
-        m.setTitle(title);
-
-        String content = "hello";//ここの値は入力できるように後でコード変更
-        m.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        m.setCreated_at(currentTime);
-        m.setUpdated_at(currentTime);
-
-        //データベースに保存
-        em.persist(m);//データベースに保存
-        em.getTransaction().commit();//データの新規登録を確定(コミット）させる命令
-
-        //自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
     }
 
 }
